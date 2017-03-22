daef12097fdf29f7b9cf8151bd9b38e102ee9ace
ankitkumar2605 <ankit.kumar1@tothenew.com>
2017-03-22 14:20:36 +0530

    with regex

--- src/com/bootcamp/ttn/TTNCafe.java
+++ src/com/bootcamp/ttn/TTNCafe.java
@@ -6,16 +6,12 @@ public class TTNCafe {
     public static void main(String[] args) {
         User admin = new Admin();
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter user ID : ");
-        String id = sc.next();
+        System.out.print("Enter user name : ");
+        String name = sc.nextLine();
 
         System.out.print("Enter user password : ");
-        String password = sc.next();
-        boolean userAuthenticated = ValidationUtils.authenticateID(admin,id,password);
-        if(userAuthenticated){
-            System.out.println("User Logged In.........");
-        }else{
-            System.out.println("User ID or Password is Invalid!!!!!!");
-        }
+        String password = sc.nextLine();
+        ValidationUtils.authenticateID(admin, name, password);
     }
+
 }
--- src/com/bootcamp/ttn/ValidationUtils.java
+++ src/com/bootcamp/ttn/ValidationUtils.java
@@ -3,14 +3,52 @@ package com.bootcamp.ttn;
 import java.util.regex.Pattern;
 
 public class ValidationUtils {
-    public static boolean authenticateID(User user, String id, String password){
-        if(Pattern.matches("\\d{6}",id)){
-            if(id.equals(user.getId())){
-                if(password.equals(user.getPassword())){
+
+    public static boolean authenticateID(User user, String name, String password){
+
+        if(name.indexOf('@')!=-1) {
+
+            if(Pattern.matches("[a-zA-Z0-9.]+@[a-zA-Z0-9]+.[a-zA-Z0-9]{3}",name)) {
+
+                if(ValidationUtils.isPasswordValid(password))
+                {
+                    System.out.println("User Logged in...");
                     return true;
                 }
+                else {
+                    System.out.println("Please enter correct password");
+                    return false;
+                }
+
             }
+            else {
+                System.out.println("Please enter valid username or emailid");
+                return false;
+            }
+
         }
+
+        else if(Pattern.matches("[[a-zA-Z]+[0-9]]{6,15}",name)) {
+
+            if(ValidationUtils.isPasswordValid(password))
+            {
+                System.out.println("User Logged in...");
+                return true;
+            }
+            else {
+                System.out.println("Please enter correct password");
+                return false;
+            }
+
+        }
+
+        else
+            System.out.println("Please enter valid username or emailid");
         return false;
     }
+
+    public static boolean isPasswordValid(String password) {
+        return Pattern.matches("^[a-zA-Z0-9_]{7,}$", password);
+    }
+
 }
